1e897782046cffcdf852b34efd1558b720616f71
Richard Li <rlii0102@student.monash.edu>
2025-03-08 18:34:33 +1100

    Implement UserController
    - implement code to interact with client
    - define calls to service layer

---
+++ src/main/java/com/richard/chatapp/controllers/UserController.java
@@ -0,0 +1,38 @@
+package com.richard.chatapp.controllers;
+
+
+import com.richard.chatapp.service.UserService;
+import com.richard.chatapp.user.User;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserController {
+  private final UserService service;
+
+  @MessageMapping("/user.addUser")
+  @SendTo("/user/topic")
+  public User addUser(@Payload User user) {
+    service.saveUser(user);
+    return user;
+  }
+
+  @MessageMapping("/user.disconnectUser")
+  @SendTo("/user/topic")
+  public User disconnect(@Payload User user) {
+    service.disconnect(user);
+    return user;
+  }
+
+  @GetMapping("/users")
+  public ResponseEntity<List<User>> findOnlineUsers(){
+    return ResponseEntity.ok(service.findOnlineUsers());
+  }
+}
